d37a616b5cf3aa49dc7b74acc1d7ff9d62460dc5
Brett Earle <brettearle@gmail.com>
2024-02-14 22:35:10 +1030

    html response, added but not sent over the wire

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -21,6 +22,13 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
+    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+    let read_to_string = fs::read_to_string("hello.html");
+    let contents = read_to_string.unwrap();
+    let length = contents.len();
+
+    let response = format! {"{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"};
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
 
