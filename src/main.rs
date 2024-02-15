5a4c2ad7bcd5bb4dce647ea28aca6c82650c0cb7
Brett Earle <brettearle@gmail.com>
2024-02-15 12:47:28 +1030

    refactored handle_connection

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,16 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let read_to_string = fs::read_to_string("hello.html");
-        let contents = read_to_string.unwrap();
-        let length = contents.len();
-
-        let response = format! {"{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"};
-
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let read_to_string = fs::read_to_string(filename);
+    let contents = read_to_string.unwrap();
+    let length = contents.len();
 
-        let response = format! {"{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"};
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    stream.write_all(response.as_bytes()).unwrap();
 }
